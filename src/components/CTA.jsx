c18f14c72239511662b3553794ac159079b8a592
Lakshya Singh <singhlakshya56@gmail.com>
2024-04-15 16:34:59 +0530

    add : sections

---
+++ src/components/CTA.jsx
@@ -0,0 +1,20 @@
+import styles from "../style";
+import Button from "./Button";
+
+const CTA = () => (
+  <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+    <div className="flex-1 flex flex-col">
+      <h2 className={styles.heading2}>Experience Our Services Today!</h2>
+      <p className={`${styles.paragraph} max-w-[500px] mt-5`}>
+        We provide all the necessary tools to accept card payments and enhance your business operations worldwide. 
+        Join us now to experience our comprehensive services.
+      </p>
+    </div>
+
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button />
+    </div>
+  </section>
+);
+
+export default CTA;
